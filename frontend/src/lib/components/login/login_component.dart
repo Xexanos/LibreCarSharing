6798354b6ea62f25b2ce6b02becc7357119dbc9c
Lars Hilgenstock <lars.hilgenstock@conciso.de>
2017-07-06 11:34:36 +0200

    added chiro integration

--- frontend/src/lib/components/login/login_component.dart
+++ frontend/src/lib/components/login/login_component.dart
@@ -12,7 +12,7 @@ import '../../model/user.dart';
 )
 class LoginComponent implements OnInit {
   User user = new User();
-  //bool isLoggedIn() => loggedIn;
+  bool loggedIn = false;
 
   @override
   Future<Null> ngOnInit() async {
@@ -22,7 +22,14 @@ class LoginComponent implements OnInit {
   void login(dynamic e){
     e.preventDefault();
     HttpRequest.postFormData("../login.jsp", { "username" : this.user.username, "password" : this.user.password })
-      //U.then((request) {loggedIn = true;})
+      .then((request) {loggedIn = true;})
       .catchError((n)=>print(n));
+  }
+
+  void logout(dynamic e){
+    e.preventDefault();
+    HttpRequest.request("../logout", method: "GET")
+        .then((request) {loggedIn = false; print(request.getAllResponseHeaders());})
+        .catchError((n)=>print(n));
   }
 }
