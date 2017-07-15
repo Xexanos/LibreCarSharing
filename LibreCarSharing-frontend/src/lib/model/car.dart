76f5f7e0a6b70fa1d1954b8ee530eaa76d603bb5
Lars Hilgenstock <lars.hilgenstock@conciso.de>
2017-07-15 20:57:40 +0200

    fixed typo

--- LibreCarSharing-frontend/src/lib/model/car.dart
+++ LibreCarSharing-frontend/src/lib/model/car.dart
@@ -3,7 +3,7 @@ import 'package:LibreCarSharingFrontend/model/user.dart';
 
 class Car {
   String name = "";
-  String typ = "";
+  String type = "";
   String location = "";
   String imageFile = "";
   bool status = true;
@@ -13,7 +13,7 @@ class Car {
   int color = 0x000000;
   String licencePlate = "";
 
-  Car(this.name, this.imageFile, this.typ, this.location, this.licencePlate);
+  Car(this.name, this.imageFile, this.type, this.location, this.licencePlate);
 
   String toJSON() {
     return JSON.encode({});
