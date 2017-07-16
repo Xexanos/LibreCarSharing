ae6074682ae755779468df7b700ca10abd9ce47a
Lars Hilgenstock <lars.hilgenstock@conciso.de>
2017-07-16 16:32:00 +0200

    removed stayLoggedIn Option

--- LibreCarSharing-frontend/src/lib/models/user.dart
+++ LibreCarSharing-frontend/src/lib/models/user.dart
@@ -5,7 +5,6 @@ class User {
   String email;
   String password;
   String imageFile;
-  bool stayLoggedIn = true;
 
   String toJSON() {
     return JSON.encode({'username':username, 'email':email, 'password':password, 'displayName':displayName});
