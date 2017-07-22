d5c53f979330492bc69e5e741c6ada50101dc15c
Xexanos <lars.hilgenstock@tu-dortmund.de>
2017-07-22 10:03:24 +0200

    redirect user to login after successful registration

--- LibreCarSharing-frontend/src/lib/components/register/register_component.dart
+++ LibreCarSharing-frontend/src/lib/components/register/register_component.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:angular2/router.dart';
 
 import 'package:LibreCarSharingFrontend/services/user_service.dart';
 import 'package:angular2/angular2.dart';
@@ -9,20 +10,29 @@ import 'package:angular2/angular2.dart';
   templateUrl: 'register_component.html',
 )
 class RegisterComponent implements OnInit {
-  String username;
-  String password;
-  String email;
+  String username = "";
+  String password = "";
+  String email = "";
 
+  final Router _router;
   final UserService _userService;
 
-  RegisterComponent(this._userService);
+  RegisterComponent(this._router, this._userService);
 
   @override
   Future<Null> ngOnInit() async {}
 
   register(dynamic e) async {
     e.preventDefault();
+
     int status = await _userService.registerUser(username, password, email);
+    switch (status){
+      case 200:
+        _router.navigate(['Login']);
+        break;
+      default:
+        print("Status code is " + status.toString());
+    }
     //TODO: react to status
   }
 }
