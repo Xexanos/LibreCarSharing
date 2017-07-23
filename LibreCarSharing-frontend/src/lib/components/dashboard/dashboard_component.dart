6040795dba1ba7316fad690412abb9139c3f8abc
Xexanos <xexanos@web.de>
2017-07-23 23:00:11 +0200

    readded page variable

--- LibreCarSharing-frontend/src/lib/components/dashboard/dashboard_component.dart
+++ LibreCarSharing-frontend/src/lib/components/dashboard/dashboard_component.dart
@@ -6,18 +6,22 @@ import 'package:LibreCarSharingFrontend/interfaces/community.dart';
 
 // Import services
 import 'package:LibreCarSharingFrontend/services/community_service.dart';
+
 @Component(
   selector: 'dashboard',
   styleUrls: const ['dashboard_component.css'],
   templateUrl: 'dashboard_component.html',
-  directives: const [BS_TABLE_DIRECTIVES,],
+  directives: const [
+    BS_TABLE_DIRECTIVES,
+  ],
 )
 class DashboardComponent implements OnInit {
   final CommunityService _communityService;
+  int page = 1;
 
   List<Community> communities = [];
 
-  DashboardComponent( this._communityService);
+  DashboardComponent(this._communityService);
 
   @override
   Future<Null> ngOnInit() async {
