9f22c11000a5ea0e3c2de58513b6650cad0edbc2
freddy41 <junkspam47@gmail.com>
2017-07-23 15:29:28 +0200

    added list du dashboard

--- LibreCarSharing-frontend/src/lib/components/dashboard/dashboard_component.dart
+++ LibreCarSharing-frontend/src/lib/components/dashboard/dashboard_component.dart
@@ -1,15 +1,36 @@
 import 'dart:async';
 
 import 'package:angular2/angular2.dart';
+import 'package:ng_bootstrap/ng_bootstrap.dart';
+import 'package:LibreCarSharingFrontend/interfaces/community.dart';
 
+// Import services
+import 'package:LibreCarSharingFrontend/services/community_service.dart';
 @Component(
   selector: 'dashboard',
   styleUrls: const ['dashboard_component.css'],
   templateUrl: 'dashboard_component.html',
+  directives: const [BS_TABLE_DIRECTIVES,],
 )
 class DashboardComponent implements OnInit {
+  final RouteParams _routeParams;
+
+  final CommunityService _communityService;
+  DashboardComponent( this._communityService);
+  List<Community> communities = [];
+  int page = 1;
+
   @override
   Future<Null> ngOnInit() async {
-    // TODO: implement ngOnInit
+    this.getcommunities();
+  }
+
+
+
+
+  getcommunities() {
+    _communityService.getCommunities().then((communities) async {
+      this.communities = communities;
+    });
   }
 }
